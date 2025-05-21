aee8ed85304b070d4a922e769946fa12279466aa
Mr_Taki <2028566723@qq.com>
2025-05-21 12:16:09 +0800

    src: 创建游戏,完成基础的模式

--- flappy_bird/src/main.rs
+++ flappy_bird/src/main.rs
@@ -1,13 +1,72 @@
+use std::str::EncodeUtf16;
+
 use bracket_lib::prelude::*;
 
+enum GameMode{
+    Menu,
+    Playing,
+    End,
+}
 // State用于保存每一帧的状态
-struct State{}
+struct State{
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm){
+        ctx.print_centered(10,"test");
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self){
+        self.mode = GameMode::Playing;
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls(); 
+        ctx.print_centered(5, "Welcome to Flappy Bird");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key{
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm){
+        ctx.cls(); 
+        ctx.print_centered(5, "Game End");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key{
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+        
+    }
+}
 
 impl GameState for State{
     // ctx = context 上下文, 此处相当于游戏窗口
    fn tick(&mut self, ctx: &mut BTerm){
-    ctx.cls();//清理屏幕
-    ctx.print(1 ,1 ,"Hello, Bracket Terminal!");//(x,y)为屏幕坐标,左上角为(0,0), 打印一句话到屏幕中
+    match self.mode {
+        GameMode::Menu => self.main_menu(ctx), 
+        GameMode::End => self.dead(ctx),
+        GameMode::Playing => self.play(ctx),
+    }
    } 
 }
 fn main() -> BError {
@@ -15,7 +74,6 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Bird")
         .build()?; // 文末的 ? 表示构建可能出错,如果出错就返回BErr
-
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
 
